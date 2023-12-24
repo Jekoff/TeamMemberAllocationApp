57a07b470904370ea45dba10e1466b4594da7cfb
Jekoff <99019668+Jekoff@users.noreply.github.com>
2023-12-24 21:34:56 +0000

    "commit"

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import * as React from 'react';
 import './App.css'
+import Header from './Header'
+import Content from './Content'
+import Footer from './Footer'
 
 export default function App() {
   return (
-    <main>
-      Hello World!
-    </main>
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
   )
 }
+
---
+++ src/Footer.jsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+  return (
+    <Footer>
+    <h1>Footer</h1>
+    </Footer>
+  ) 
+}
+export default Footer
+
+
+const Footer = () => {
+  return (
+    <Footer>
+    <h1>Footer</h1>
+    </Footer>
+  ) 
+}
+export default Footer
